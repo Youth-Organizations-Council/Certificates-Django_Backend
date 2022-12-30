fe8cf0463f9352dca30810692efc9e22529044b3
Arpan Pandey <arpan@hackersreboot.tech>
2022-12-30 14:22:22 +0530

    Henlo DB

--- yoc_certificates_backend/settings.py
+++ yoc_certificates_backend/settings.py
@@ -82,13 +82,25 @@ WSGI_APPLICATION = "yoc_certificates_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "yoc_certs",
+            "USER": "root",
+            "PASSWORD": "aerotoor",
+            "HOST": "165.22.214.195",
+            "PORT": os.environ.get("SQL_PORT", "5432"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
